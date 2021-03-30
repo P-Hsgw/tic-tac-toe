5d0888cc50780b59dbf5537cf58d9f3f6c777b65
P-Hsgw <contact@p-hsgw.com>
2021-03-30 17:23:13 +0900

    Add a  function that renders the grid and occupy it with array content

--- app.js
+++ app.js
@@ -1,26 +1,35 @@
+// array should be populated whenever user clicks on corresponding div 
+
+// Plan player rounds - X goes first, O goes second
+// Add event listeners to occupy the array when div is clicked
+//Can't occupy same array with two different symbols 
+
 const gameBoard = (() => {
-  const containers = document.querySelector(".container")
-  const board = ["", "", "", "", "", "", "", "", ""];
-  let i = 0
-  for (i=0; i < board.length; i++) {
-    let newBox = document.createElement("div")
-    newBox.classList.add("field")
-    newBox.setAttribute("id", `${i}`);
-    containers.appendChild(newBox);
-  }
-
+  const board = new Array(9).fill("X");
   
+  const createBoard = (() => {
+    const gridContainer = document.querySelector(".grid-container")
+    let i = 0
+    for (i=0; i < board.length; i++) {
+      let newBox = document.createElement("div")
+      newBox.classList.add("container")
+      newBox.classList.add(`container_${i}`)
+      newBox.setAttribute("id", `${i}`);
+      newBox.innerHTML = board[i]
+      gridContainer.appendChild(newBox);
+    }
+  })()
+  const winningCombination = []
 
-
-
-  const winningCombination = ""
-
+  return {
+    createBoard,
+    board
+  };
 })();
 
 // const displayControler (() => {
 
 // })();
-
 const Player = (name, symbol) => {
   return {
     name,
