cd8ac0d19f2b992b0ffb7c3cb47fbc7b714dafc7
P-Hsgw <contact@p-hsgw.com>
2021-03-30 18:04:31 +0900

    Add populateBoard function
    This commit adds populateBoard function, that fills board array depending on user choice, and displays the choice on a chosen field.

--- app.js
+++ app.js
@@ -5,31 +5,41 @@
 //Can't occupy same array with two different symbols 
 
 const gameBoard = (() => {
-  const board = new Array(9).fill("X");
-  
+  let board = new Array(9).fill(null);
   const createBoard = (() => {
     const gridContainer = document.querySelector(".grid-container")
     let i = 0
     for (i=0; i < board.length; i++) {
-      let newBox = document.createElement("div")
-      newBox.classList.add("container")
-      newBox.classList.add(`container_${i}`)
+      let newBox = document.createElement("div");
+      newBox.classList.add("container");
+      newBox.classList.add(`container_${i}`);
       newBox.setAttribute("id", `${i}`);
-      newBox.innerHTML = board[i]
+      newBox.dataset.index = i
+      newBox.innerHTML = board[i];
       gridContainer.appendChild(newBox);
     }
-  })()
+  })();
   const winningCombination = []
 
   return {
-    createBoard,
     board
   };
+
 })();
 
-// const displayControler (() => {
+const displayController = (() => {
+  const populateBoard = (() => {
+    const allFields = document.querySelectorAll(".container")
+    allFields.forEach(field => {
+     field.addEventListener("click", () => {
+      gameBoard.board[field.dataset.index] = "X"
+      field.innerHTML = gameBoard.board[field.dataset.index];
+      })
+    })
+  })()
+  
+})();
 
-// })();
 const Player = (name, symbol) => {
   return {
     name,
