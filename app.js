37c565fd06f713cc6131eb4cdcec34d95b4b24f7
P-Hsgw <contact@p-hsgw.com>
2021-04-01 17:00:47 +0900

    Run Prettier to make the code more readable

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const gameBoard = (() => {
   let board = new Array(9).fill(null);
   // Render game board at the start of the game
   const renderBoard = (() => {
-    const gridContainer = document.querySelector(".grid-container")
-    let i = 0
-    for (i=0; i < board.length; i++) {
+    const gridContainer = document.querySelector(".grid-container");
+    let i = 0;
+    for (i = 0; i < board.length; i++) {
       let newBox = document.createElement("div");
       newBox.classList.add("container");
       newBox.classList.add(`container_${i}`);
       newBox.setAttribute("id", `${i}`);
-      newBox.dataset.index = i
+      newBox.dataset.index = i;
       newBox.innerHTML = board[i];
       gridContainer.appendChild(newBox);
     }
@@ -17,54 +17,30 @@ const gameBoard = (() => {
 
   // Return true if winning combination is met
   let winningCombination = (symbol) => {
-    if (board[0] === symbol &&
-    board[1] === symbol &&
-    board[2] === symbol ||
-    board[3] === symbol &&
-    board[4] === symbol &&
-    board[5] === symbol ||
-    board[6] === symbol &&
-    board[7] === symbol &&
-    board[8] === symbol ||
-    board[0] === symbol &&
-    board[3] === symbol &&
-    board[6] === symbol ||
-    board[1] === symbol &&
-    board[4] === symbol &&
-    board[7] === symbol ||
-    board[2] === symbol &&
-    board[5] === symbol &&
-    board[8] === symbol ||
-    board[0] === symbol &&
-    board[4] === symbol &&
-    board[8] === symbol ||
-    board[2] === symbol &&
-    board[4] === symbol &&
-    board[6] === symbol ||
-    board[6] === symbol &&
-    board[7] === symbol &&
-    board[8] === symbol ||
-    board[0] === symbol &&
-    board[3] === symbol &&
-    board[6] === symbol ||
-    board[1] === symbol &&
-    board[4] === symbol &&
-    board[7] === symbol ||
-    board[2] === symbol &&
-    board[5] === symbol &&
-    board[8] === symbol
+    if (
+      (board[0] === symbol && board[1] === symbol && board[2] === symbol) ||
+      (board[3] === symbol && board[4] === symbol && board[5] === symbol) ||
+      (board[6] === symbol && board[7] === symbol && board[8] === symbol) ||
+      (board[0] === symbol && board[3] === symbol && board[6] === symbol) ||
+      (board[1] === symbol && board[4] === symbol && board[7] === symbol) ||
+      (board[2] === symbol && board[5] === symbol && board[8] === symbol) ||
+      (board[0] === symbol && board[4] === symbol && board[8] === symbol) ||
+      (board[2] === symbol && board[4] === symbol && board[6] === symbol) ||
+      (board[6] === symbol && board[7] === symbol && board[8] === symbol) ||
+      (board[0] === symbol && board[3] === symbol && board[6] === symbol) ||
+      (board[1] === symbol && board[4] === symbol && board[7] === symbol) ||
+      (board[2] === symbol && board[5] === symbol && board[8] === symbol)
     ) {
-      return true
+      return true;
     } else {
-      return false
+      return false;
     }
-  }
+  };
 
   return {
     board,
-    winningCombination
+    winningCombination,
   };
-
 })();
 
 const displayController = (() => {
@@ -72,43 +48,46 @@ const displayController = (() => {
 
   // Add event listeners that populate the board array depending on players round. If gameEnded = true, don't trigger the listeners.
   const populateBoard = (() => {
-    const allFields = document.querySelectorAll(".container")
-    allFields.forEach(field => {
+    const allFields = document.querySelectorAll(".container");
+    allFields.forEach((field) => {
       field.addEventListener("click", () => {
-      if (gameBoard.board[field.dataset.index] === null && gameEnded === false) {
-        if (player1.round) {
-          gameBoard.board[field.dataset.index] = "X"
+        if (
+          gameBoard.board[field.dataset.index] === null &&
+          gameEnded === false
+        ) {
+          if (player1.round) {
+            gameBoard.board[field.dataset.index] = "X";
+          }
+          if (player2.round) {
+            gameBoard.board[field.dataset.index] = "O";
+          }
+          player1.round = !player1.round;
+          player2.round = !player2.round;
+          field.innerHTML = gameBoard.board[field.dataset.index];
+          checkWinner();
+        } else {
+          return;
         }
-        if (player2.round) {
-          gameBoard.board[field.dataset.index] = "O"
-        }
-        player1.round = !player1.round
-        player2.round = !player2.round
-        field.innerHTML = gameBoard.board[field.dataset.index];
-        checkWinner()
-      } else {
-        return;
-      }
-      })
-    })
-  })()
+      });
+    });
+  })();
 
   // Check if winningCombination is met. If yes - returns innerHTML and gameEnded = true
-  const checkWinner =() => {
-    const displayResult = document.getElementById("result")
+  const checkWinner = () => {
+    const displayResult = document.getElementById("result");
     if (gameBoard.winningCombination(player1.symbol)) {
-      displayResult.innerHTML = `The winner is... ${player1.name}`
+      displayResult.innerHTML = `The winner is... ${player1.name}`;
       gameEnded = true;
     }
     if (gameBoard.winningCombination(player2.symbol)) {
-      displayResult.innerHTML = `The winner is... ${player2.name}`
+      displayResult.innerHTML = `The winner is... ${player2.name}`;
       gameEnded = true;
     }
-    if (gameBoard.board.every(symbol => symbol != null)) {
-      displayResult.innerHTML = `It's a tie`
+    if (gameBoard.board.every((symbol) => symbol != null)) {
+      displayResult.innerHTML = `It's a tie`;
       gameEnded = true;
     }
-  }
+  };
 })();
 
 const Player = (name, symbol, round) => {
@@ -119,5 +98,5 @@ const Player = (name, symbol, round) => {
   };
 };
 
-const player1 = Player ("Patryk", "X", true);
-const player2 = Player ("Marino", "O", false);
+const player1 = Player("Patryk", "X", true);
+const player2 = Player("Marino", "O", false);
